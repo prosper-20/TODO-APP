324a69c1b53b0345781ffa289fd22e4f26d17ecf
prosper-20 <edwardprosper001@gmail.com>
2022-01-24 19:13:58 +0100

    updates

--- main/views.py
+++ main/views.py
@@ -12,4 +12,8 @@ class TaskDetailView(DetailView):
     model = Task
     template_name = "main/task_detail.html"
     context_object_name = "task"
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = "__all__"
     
