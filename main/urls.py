7bf5166aa8d91dcf9e50544aa2e1927c7ad70ee5
prosper-20 <edwardprosper001@gmail.com>
2022-01-24 20:03:49 +0100

    updates

--- main/urls.py
+++ main/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('', TaskView.as_view(), name="home"),
     path('task-detail/<slug>/', TaskDetailView.as_view(), name="detail"),
     path("task-create/", TaskCreateView.as_view(), name="create"),
-    path("task-update/<slug>/", TaskUpdateView.as_view(), name="update"),
-    path("task-delete/<slug>/", TaskDeleteView.as_view(), name="delete")
+    # path("task-update/<slug>/", TaskUpdateView.as_view(), name="update"),
+    # path("task-delete/<slug>/", TaskDeleteView.as_view(), name="delete")
 
 
 ]
