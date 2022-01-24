b2a79424871de433d0c3f3606f30532247ff6cc5
prosper-20 <edwardprosper001@gmail.com>
2022-01-24 12:56:06 +0100

    updates

--- main/models.py
+++ main/models.py
@@ -4,3 +4,13 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title 
+
+    class Meta:
+        ordering = ["completed"]
