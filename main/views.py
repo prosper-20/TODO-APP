31839a8d659a985dd6c69a64707db53e9e11c2fb
prosper-20 <edwardprosper001@gmail.com>
2022-01-24 19:05:45 +0100

    updates

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Task
 
 
@@ -11,5 +11,5 @@ class TaskView(ListView):
 class TaskDetailView(DetailView):
     model = Task
     template_name = "main/task_detail.html"
-    context_object_name = "tasks"
+    context_object_name = "task"
     
