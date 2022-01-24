23badf0bbcdb386cacb45cb67fb880dde2ab7c5e
prosper-20 <edwardprosper001@gmail.com>
2022-01-24 19:22:31 +0100

    updates

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskView, TaskDetailView
+from .views import TaskView, TaskDetailView, TaskCreateView
 
 urlpatterns = [
     path('', TaskView.as_view(), name="home"),
     path('task-detail/<slug>/', TaskDetailView.as_view(), name="detail"),
+    path("task-create/", TaskCreateView.as_view(), name="task_create")
 
 ]
